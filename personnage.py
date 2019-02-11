53a58a315f8b316337fdcd1a36e20e20eccc3d40
Verrier <samuel.boussus@gmail.com>
2019-02-11 10:14:37 +0100

    création d'un objet de la classe personnage avec saisie des variables nom, prenom, age

--- personnage.py
+++ personnage.py
@@ -1,11 +1,20 @@
 # -*-coding:utf8-*
 import os # On importe le module os
 
+import random
+
 class Personnage:
 	def __init__(self, nom, prenom, age):
 		"""Seul le nom, le prénom et l'age sont définis lors de la création du personnage."""
 		self.nom = nom
 		self.prenom = prenom
 		self.age = age
+
+		
+nom = input("Nom:")
+prenom = input("Prénom:")
+age = random.randrange(0,100)
+
+personnage = Personnage(nom,prenom,age)
 		
 os.system("pause")
