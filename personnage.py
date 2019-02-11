f386bed52f891977a8df4d1c55013d71ee666079
Verrier <samuel.boussus@gmail.com>
2019-02-11 10:04:25 +0100

    création de la classe personnage

--- personnage.py
+++ personnage.py
@@ -1,6 +1,11 @@
 # -*-coding:utf8-*
 import os # On importe le module os
 
-
- 
+class Personnage:
+	def __init__(self, nom, prenom, age):
+		"""Seul le nom, le prénom et l'age sont définis lors de la création du personnage."""
+		self.nom = nom
+		self.prenom = prenom
+		self.age = age
+		
 os.system("pause")
