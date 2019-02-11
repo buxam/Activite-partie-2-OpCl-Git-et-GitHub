5df396c75ff7d0de4dc4f2a70e73ebd5ae06cc10
Verrier <samuel.boussus@gmail.com>
2019-02-11 10:22:06 +0100

    ajout des methodes d'affichage de la classe personnage

--- personnage.py
+++ personnage.py
@@ -9,12 +9,19 @@ class Personnage:
 		self.nom = nom
 		self.prenom = prenom
 		self.age = age
-
+	def __repr__(self):
+		"""Représentation de l'objet dans l'interpréteur."""
+		return "{} {} a {} ans.".format(self.prenom, self.nom, self.age)
+	def __str__(self):
+		"""Représentation de l'objet sous forme d'une chaîne de caractères."""
+		return repr(self)
 		
 nom = input("Nom:")
 prenom = input("Prénom:")
-age = random.randrange(0,100)
+age = random.randrange(10,100)
 
 personnage = Personnage(nom,prenom,age)
+
+print (personnage)
 		
 os.system("pause")
